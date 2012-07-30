cefe6944469fb86e7e6ff11fe34665638b2ed459
Nat <nat@raspberrypi.(none)>
2012-07-30 23:50:29 +0100

    chown GPIO files to actual user that invoked gpio-admin

--- src/gpio-admin.c
+++ src/gpio-admin.c
@@ -23,15 +23,15 @@ void usage_error(char **argv) {
   exit(1);
 }
 
-void allow_access_by_group(const char *pin_str, const char *filename) {
+void allow_access_by_user(const char *pin_str, const char *filename) {
   char path[PATH_MAX];
   snprintf(path, PATH_MAX, "/sys/devices/virtual/gpio/gpio%s/%s", pin_str, filename);
   
-  if (chown(path, -1, getegid()) != 0) {
+  if (chown(path, getuid(), getgid()) != 0) {
     error(5, errno, "failed to change group ownership of %s", path);
   }
   
-  if (chmod(path, S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP) != 0) {
+  if (chmod(path, S_IRUSR|S_IWUSR) != 0) {
     error(6, errno, "failed to set permissions of %s", path);
   }
 }
@@ -80,9 +80,9 @@ int main(int argc, char **argv) {
   
   if (strcmp(command, "export") == 0) {
     write_text_to_path(GPIO_EXPORT_PATH, pin_str);
-    allow_access_by_group(pin_str, "direction");
-    allow_access_by_group(pin_str, "value");
-    allow_access_by_group(pin_str, "active_low");
+    allow_access_by_user(pin_str, "direction");
+    allow_access_by_user(pin_str, "value");
+    allow_access_by_user(pin_str, "active_low");
   }
   else if (strcmp(command, "unexport") == 0) {
     write_text_to_path(GPIO_UNEXPORT_PATH, pin_str);
